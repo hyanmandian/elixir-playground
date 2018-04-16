1afc6fd447a130f4b258700971ab3009971553a8
Hyan Mandian <hyanmandian@hotmail.com>
2018-04-16 14:02:25 -0300

    remove unnecessary parentheses

--- challenges/arrays-ds/readme.exs
+++ challenges/arrays-ds/readme.exs
@@ -2,7 +2,7 @@
 
 defmodule ArraysDs do
   def parse(input) do
-    [head, tail] = input |> String.trim() |> String.split("\n")
+    [head, tail] = input |> String.trim |> String.split("\n")
     [head, String.split(tail)]
   end
 end
